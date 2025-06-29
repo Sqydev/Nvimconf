a8d3bcbc737232729483b89601de293a4d6758f3
Sqyd <wojkap10@proton.me>
2025-06-29 16:07:45 +0200

    QWDRV

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,48 +1,67 @@
+-- Failuje instalacje clangd. Jak znowu przestanie ci complex.h działać to tym razem zrób żeby w ogóle nie próbowało instalować clangd
+
 local M = {
-	'williamboman/mason.nvim',
-	dependencies = {
+    'williamboman/mason.nvim',
+    dependencies = {
       'williamboman/mason-lspconfig.nvim',
       'nvim-lua/plenary.nvim',
-	},
+    },
 }
 
 M.servers = {
-	"lua_ls",
-	"rust_analyzer",
-	"clangd",
+    "lua_ls",
+    "rust_analyzer",
+    --"clangd",
 }
 
+
 function M.config()
-	local lsp = require("lsp-zero")
-	lsp.preset("recommended")
+    local lsp = require("lsp-zero")
 
-	lsp.set_preferences({
-		suggest_lsp_servers = false,
-		sign_icons = {
-			error = '✖',
-			warn = '▲',
-			hint = 'H',
-			info = 'I'
-		}
-	})
+    lsp.preset("recommended")
 
-	require "lsp-zero".setup()
-	require("mason").setup()
+    lsp.set_preferences({
+        suggest_lsp_servers = false,
+        sign_icons = {
+            error = '✖',
+            warn = '▲',
+            hint = 'H',
+            info = 'I'
 
-	require "mason-lspconfig".setup_handlers {
-		function (server_name)
-		    require "lspconfig" [server_name].setup {}
-		end
-	}
+        }
 
-	require("mason-lspconfig").setup {
-		ensure_installed = M.servers,
-      		automatic_installation = true,
-	}
+    })
 
-	vim.diagnostic.config({
-		virtual_text = true
-	})
+    require("mason").setup()
+
+    require("mason-lspconfig").setup {
+        ensure_installed = M.servers,
+          automatic_installation = true,
+        exclude = { "clangd" },
+
+    }
+
+    require "lsp-zero".setup()
+
+    local lspconfig = require('lspconfig')
+    lspconfig.clangd.setup({
+        cmd = { "clangd", "--query-driver=/run/current-system/sw/bin/gcc" },
+        mason = false,
+        on_attach = lsp.on_attach,
+        capabilities = lsp.capabilities,
+    })
+
+    require "mason-lspconfig".setup_handlers {
+        function (server_name)
+            require "lspconfig" [server_name].setup {}
+        end
+    }
+
+    lsp.setup()
+
+    vim.diagnostic.config({
+        virtual_text = true
+    })
 
 end
 
